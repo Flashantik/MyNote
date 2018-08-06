1a7720157e287e8b72960a455096d6e3b7636014
Flashantik <Flashantik@gmail.com>
2018-08-06 16:03:01 +0300

    updated

---
+++ src/components/note.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <v-container >
+    <quill-editor class="notepad" 
+        :content="content"
+        @change="onEditorChange($event)">
+    </quill-editor>
+  </v-container>
+</div>
+</template>
+
+<script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import { quillEditor } from 'vue-quill-editor'
+
+export default{
+  components: {
+    quillEditor
+  },
+  data () {
+    return {
+      content: ''
+    }
+  },
+  methods: {
+    onEditorChange ({ quill, html, text }) {
+      this.content = html
+    }
+  }
+}
+</script>
+<style scoped>
+
+.notepad{
+  max-height: 85vh;
+}
+
+</style>
